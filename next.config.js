8dcec8b8f137571ed73f810f9a07ee95c27012ff
marcospss <marcospss@gmail.com>
2021-01-17 20:12:36 -0300

    feat: install and Config @module-federation/nextjs-mf

---
+++ next.config.js
@@ -0,0 +1,32 @@
+const {
+  withModuleFederation,
+  MergeRuntime,
+} = require("@module-federation/nextjs-mf");
+const path = require("path");
+
+module.exports = {
+  webpack: (config, options) => {
+    const { isServer } = options;
+    const microfrontendConfig = {
+      name: "article",
+      library: { type: config.output.libraryTarget, name: "article" },
+      filename: "static/runtime/remoteEntry.js",
+      remotes: {},
+      exposes: {
+        './Article': './src/views/Article'
+      },
+      shared: [],
+    };
+
+    // Configures ModuleFederation and other Webpack properties
+    withModuleFederation(config, options, microfrontendConfig);
+
+    config.plugins.push(new MergeRuntime());
+
+    if (!isServer) {
+      config.output.publicPath = "http://localhost:3011/_next/";
+    }
+
+    return config;
+  },
+};
---
+++ src/views/Article/index.tsx
@@ -0,0 +1,5 @@
+const Article = (): JSX.Element => {
+  return <h1>Articles Total Read</h1>;
+};
+
+export default Article;
