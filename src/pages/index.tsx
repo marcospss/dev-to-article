633d198b94b2a296e89566812749094340d4dcb5
marcospss <marcospss@gmail.com>
2021-01-31 17:15:16 -0300

    fix: ajust script files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,18 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 
-import Head from '~/infrastructure/components/Head';
-
+import HeadNext from '~/infrastructure/components/Head';
 const IndexPage: NextPage = () => {
   return (
     <>
-      <Head title="Article" />
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js TypeScript @dev-to Article!</a>
-        </h1>
-        <Link
-          href={`/[username]/[slug]`}
-          as={`/hack4impact/a-shiny-on-hover-effect-that-follows-your-mouse-css-4d5b`}>
-          <a>Details</a>
-        </Link>
-      </main>
+      <HeadNext title="Home" />
+      <h1>Article</h1>
+      <hr />
+      <Link
+        href={`/[username]/[slug]`}
+        as={`/hack4impact/a-shiny-on-hover-effect-that-follows-your-mouse-css-4d5b`}>
+        <a>Details</a>
+      </Link>
     </>
   );
 };
